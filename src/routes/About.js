45a4597b633e7808e64b4d74341d4c2c06e03475
AlissonHanael <90325162+AlissonHanael@users.noreply.github.com>
2022-08-02 14:12:47 -0300

    versao 1.0.2

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,14 +1,19 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const about = () => {
-	return (
-		<div>
-			<Navbar />
-			<Footer />
-		</div>
-	)
+  return (
+    <div>
+      <Navbar />
+      <HeroImg2
+        heading="SOBRE"
+        text="Aqui você encontra um pouco mais da minha história"
+      />
+      <Footer />
+    </div>
+  )
 }
 
 export default about
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,14 +1,19 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const project = () => {
-	return (
-		<div>
-			<Navbar />
-			<Footer />
-		</div>
-	)
+  return (
+    <div>
+      <Navbar />
+      <HeroImg2
+        heading="PROJETOS"
+        text="Confira aqui os projetos que já fiz."
+      />
+      <Footer />
+    </div>
+  )
 }
 
 export default project
